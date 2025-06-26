45b7b63689ef6aaf69a3167fc256e685cc6202f2
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2025-06-26 17:41:50 +0700

    fix(main): fixing main.py

--- main.py
+++ main.py
@@ -4,31 +4,32 @@ from contextlib import asynccontextmanager
 from app.core.config import settings
 from app.core.db import connect_to_mongo, close_mongo_connection
 
-from app.api import auth, users, facilities, expenses, microfunding
+from app.api import auth, users, facilities, expense, microfunding
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("--- Memulai Aplikasi Danaraga API ---")
+    print("Memulai Danaraga API")
     await connect_to_mongo()
     yield
     await close_mongo_connection()
-    print("--- Aplikasi Danaraga API Telah Berhenti ---")
+    print("Danaraga API Telah Berhenti")
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
-    docs_url=f"{settings.API_V1_STR}/docs",
-    redoc_url=f"{settings.API_V1_STR}/redoc"
+    docs_url=f"{settings.API_V1_STR}/docs",         
+    redoc_url=f"{settings.API_V1_STR}/redoc",       
+    lifespan=lifespan                             
 )
 
-print("Mendaftarkan router...")
+print("Mendaftarkan router")
 app.include_router(auth.router, prefix=f"{settings.API_V1_STR}/auth", tags=["Authentication"])
 app.include_router(users.router, prefix=f"{settings.API_V1_STR}/users", tags=["Users"])
-app.include_router(expenses.router, prefix=f"{settings.API_V1_STR}/expenses", tags=["Expenses"])
+app.include_router(expense.router, prefix=f"{settings.API_V1_STR}/expenses", tags=["Expenses"])
 app.include_router(facilities.router, prefix=f"{settings.API_V1_STR}/facilities", tags=["Facilities"])
 app.include_router(microfunding.router, prefix=f"{settings.API_V1_STR}/microfunding", tags=["Microfunding"])
 print("Semua router berhasil didaftarkan.")
 
 @app.get("/", tags=["Root"])
-def read_root():
-    return {"message": f"Welcome to {settings.PROJECT_NAME}"}
+async def read_root():
+    return {"message": f"Welcome to {settings.PROJECT_NAME}. Visit {settings.API_V1_STR}/docs for documentation."}
