d20f080df815d38f7d2040023f16d3ad7b1292af
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2025-06-26 18:27:46 +0700

    fix(model): fixing models so the documentation works

--- app/models/common.py
+++ app/models/common.py
@@ -1,19 +1,41 @@
-from pydantic import BaseModel, Field
-from typing import Optional
+from pydantic import BaseModel, Field, GetCoreSchemaHandler
+from pydantic_core import CoreSchema, core_schema
+from bson import ObjectId
+from typing import Any, Optional
 
 class PyObjectId(str):
     @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
+    def __get_pydantic_core_schema__(
+        cls, source_type: Any, handler: GetCoreSchemaHandler
+    ) -> CoreSchema:
+        return core_schema.json_or_python_schema(
+            json_schema=core_schema.str_schema(),
+            python_schema=core_schema.union_schema(
+                [
+                    core_schema.is_instance_schema(ObjectId),
+                    core_schema.chain_schema(
+                        [
+                            core_schema.str_schema(),
+                            core_schema.no_info_plain_validator_function(cls.validate),
+                        ]
+                    ),
+                ]
+            ),
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                lambda x: str(x)
+            ),
+        )
 
     @classmethod
-    def validate(cls, v, *args, **kwargs):
-        if not isinstance(v, str) or not len(v) == 24: 
-             pass 
-        return str(v)
+    def validate(cls, v: str) -> ObjectId:
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid ObjectId")
+        return ObjectId(v)
 
 class IDModelMixin(BaseModel):
     id: Optional[PyObjectId] = Field(alias="_id", default=None)
     
     class Config:
-        populate_by_name = True  
+        from_attributes = True 
+        populate_by_name = True
+        json_encoders = {ObjectId: str} 
